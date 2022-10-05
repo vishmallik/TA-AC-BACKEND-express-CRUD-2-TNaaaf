836c38bd300af0e933ab6e48b536432ae96dcd9e
visawjeet mallik <vishmallik@hotmail.com>
2022-10-05 21:23:04 +0530

    express associations assignment 2

---
+++ block-BNaadm/blog/models/Article.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const articleSchema = new Schema({
+  title: { type: String, required: true },
+  description: String,
+  tags: [String],
+  author: String,
+  likes: { type: Number, default: 0 },
+  comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+});
+
+module.exports = mongoose.model("Article", articleSchema);
